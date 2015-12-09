5130f1d28e5be237086e98f90e1d3b3b80bd57a3
bensoer <ben@soernet.ca>
2015-12-08 16:50:47 -0800

    updated documentation

--- crypto/algorithms/algorithminterface.py
+++ crypto/algorithms/algorithminterface.py
@@ -6,6 +6,12 @@ This will mimmick an interface using python's Abstract Base Class module
 https://dbader.org/blog/abstract-base-classes-in-python
 '''
 
+'''
+AlgorithmInterface is the basic interface that defines all requirements needed to be implemented by a crypto
+algorithm running on the PyChat program. Although not largely enforcable, all encryption algorithms must inherit
+from this abstract class to ensure appropriate methods needed are implemented. Failing to do so may cause failure
+in the ability to find the module
+'''
 class AlgorithmInterface(metaclass=ABCMeta):
 
     @abstractmethod
--- crypto/algorithms/caesarcipher.py
+++ crypto/algorithms/caesarcipher.py
@@ -2,6 +2,12 @@ __author__ = 'bensoer'
 
 from .algorithminterface import AlgorithmInterface
 
+'''
+CaesarCipher is an Algorithm using the CaesarCipher encryption techniques.
+
+Letters are replaced with equivelent letters in the alphabet by a certain offset off. For example A is replaced with D
+with an offset of 3. Decryption is simply reversing this process on the recieved string
+'''
 class CaesarCipher(AlgorithmInterface):
 
     def __init__(self):
