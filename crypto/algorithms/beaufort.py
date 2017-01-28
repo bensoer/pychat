691b31256c032edb5e00a79669e94cd378ffaaa2
bensoer <ben@soernet.ca>
2017-01-27 23:18:17 -0800

    added beaufort, railfence and vigener. implemented all except railfence

---
+++ crypto/algorithms/beaufort.py
@@ -0,0 +1,42 @@
+from crypto.algorithms.algorithminterface import AlgorithmInterface
+from tools.argparcer import ArgParcer
+
+class BeaufortCipher(AlgorithmInterface):
+
+    __key = ""
+
+
+    def __init__(self, arguments):
+
+        key = ArgParcer.getValue(arguments, "-k")
+        if key == "":
+            raise AttributeError("Key Is Required Parameter For Beaufort Cipher")
+        else:
+            self.__key = key
+            self.__key.upper()
+
+    def encryptString(self, unencryptedMessage):
+
+        encryptedMessage = ""
+        for index, letter in enumerate(unencryptedMessage):
+            keyIndex = index % len(self.__key)
+            keyLetter = self.__key[keyIndex]
+            offset = abs(ord(keyLetter) - ord('A'))
+            # the only difference from vigenere is here. beaufort is vigenere but with a backwards alphabet
+            encryptedLetter = chr(ord(letter) - offset)
+            encryptedMessage += encryptedLetter
+
+        return encryptedMessage.encode()
+
+    def decryptString(self, encryptedMessage):
+
+        plaintextMessage = ""
+        encryptedMessage = encryptedMessage.decode()
+        for index, letter in enumerate(encryptedMessage):
+            keyIndex = index % len(self.__key)
+            keyLetter = self.__key[keyIndex]
+            offset = abs(ord(keyLetter) - ord('A'))
+            plaintextLetter = chr(ord(letter) + offset)
+            plaintextMessage += plaintextLetter
+
+        return plaintextMessage
