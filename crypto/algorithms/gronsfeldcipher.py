6ab2d58f320a01d2fa2340ffc4fb4a22621214e4
bensoer <ben@soernet.ca>
2017-03-20 20:44:46 -0700

    implemented some of railfence. its still heavily unstable. added gronsfeldcipher

---
+++ crypto/algorithms/gronsfeldcipher.py
@@ -0,0 +1,39 @@
+from crypto.algorithms.algorithminterface import AlgorithmInterface
+from tools.argparcer import ArgParcer
+
+class GronsfeldCipher(AlgorithmInterface):
+
+    __key = ""
+
+
+    def __init__(self, arguments):
+
+        key = ArgParcer.getValue(arguments, "-k")
+        if key == "" :
+            raise AttributeError("A key value is required for the GronsfeldCipher")
+        else:
+            self.__key = key
+            self.__key.upper()
+
+    def encryptString(self, unencryptedMessage):
+
+        encryptedMessage = ""
+        for index, letter in enumerate(unencryptedMessage):
+            keyIndex = index % len(self.__key)
+            offset = int(self.__key[keyIndex])
+            encryptedLetter = chr(ord(letter) + offset)
+            encryptedMessage += encryptedLetter
+
+        return encryptedMessage.encode()
+
+    def decryptString(self, encryptedMessage):
+
+        plaintextMessage = ""
+        encryptedMessage = encryptedMessage.decode()
+        for index, letter in enumerate(encryptedMessage):
+            keyIndex = index % len(self.__key)
+            offset = int(self.__key[keyIndex])
+            plaintextLetter = chr(ord(letter) - offset)
+            plaintextMessage += plaintextLetter
+
+        return plaintextMessage
--- crypto/algorithms/railfencecipher.py
+++ crypto/algorithms/railfencecipher.py
@@ -1,3 +1,5 @@
+__author__ = 'bensoer'
+
 from crypto.algorithms.algorithminterface import AlgorithmInterface
 from tools.argparcer import ArgParcer
 
@@ -16,14 +18,121 @@ class RailFenceCipher(AlgorithmInterface):
         # at minimum there must be 1/2 - 1 rails so as to ensure some letters are scrambled
         # note this comes an issue if the message only has 2 letters in it :s - just send it plain in that case
 
+    def initializeRails(self, unencryptedMessage):
+        railList = list()
+
+        if self.__rails == -1:
+            # dynamic rails
+            railCount = int((len(unencryptedMessage) / 2) - 1)
+            for i in range(0, railCount):
+                railList.append(list())
+        else:
+            # set rails
+            for i in range(0, self.__rails):
+                railList.append(list())
+
+        return railList
+
+
     def encryptString(self, unencryptedMessage):
 
-        # TODO
+        railList = self.initializeRails(unencryptedMessage)
+        if len(railList) == 0:
+            return unencryptedMessage.encode()
+        else:
 
-        return unencryptedMessage.encode()
+            # scamble letters into their rails
+            increment = True
+            railIndex = 0
+            for index, letter in enumerate(unencryptedMessage):
+                railList[railIndex].append(letter)
+
+                if increment:
+                    railIndex += 1
+                    if railIndex >= len(railList)-1:
+                        increment = False
+                else:
+                    railIndex -= 1
+                    if railIndex <= 0:
+                        increment = True
+
+            print(railList)
+
+
+            # read out the message rail by rail
+            encryptedMessage = ""
+            for rail in railList:
+                for letter in rail:
+                    encryptedMessage += letter
+
+        return encryptedMessage.encode()
 
     def decryptString(self, encryptedMessage):
+        encryptedMessageText = encryptedMessage.decode()
+        # print(encryptedMessageText)
 
-        #TODO
+        railList = self.initializeRails(encryptedMessageText)
+        if len(railList) == 0:
+            return encryptedMessageText
+        else:
 
-        return encryptedMessage.decode()
+            # place letters back into their rails
+
+            increment = True
+            railIndex = 0 # resolves to the rail which the letter will be placed in
+            letterIndex = 0 # resolves to the letter in the encryptedMessageText we are trying to find its home to
+            for lap in range(0, len(railList)):
+                # lap resolves to the rail we are currently finding letters that belong to it
+                railIndex = lap
+
+                for travelIndex in range(lap, len(encryptedMessageText)):
+                    # travelIndex resolves to which letter through the rails we are on - it mostly has no use
+                    # but ensures that the railIndex does not keep going past as many letters as there are in the
+                    # message
+
+                    if lap == railIndex:
+
+                        '''print(lap)
+                        print(railIndex)
+                        print(len(encryptedMessageText))
+                        print(letterIndex)
+                        print(travelIndex)
+                        print(railList)'''
+                        railList[lap].append(encryptedMessageText[letterIndex
+                        '''print("MATCH MADE. ASSIGNING LETTER: " + str(
+                            encryptedMessageText[letterIndex]) + " TO LAP: " + str(lap))'''
+                        letterIndex += 1
+
+                        if letterIndex >= len(encryptedMessageText):
+                            break
+
+
+                    if increment:
+                        railIndex += 1
+                        if railIndex >= len(railList) - 1:
+                            increment = False
+                    else:
+                        railIndex -= 1
+                        if railIndex <= 0:
+                            increment = True
+
+            print(railList)
+
+
+            # run through rails to reassemble
+            unencryptedMessage = ""
+            increment = True
+            railIndex = 0
+            for letterCount in range(0, len(encryptedMessageText)):
+                unencryptedMessage += railList[railIndex].pop(0)
+
+                if increment:
+                    railIndex += 1
+                    if railIndex >= len(railList) - 1:
+                        increment = False
+                else:
+                    railIndex -= 1
+                    if railIndex <= 0:
+                        increment = True
+
+        return unencryptedMessage
