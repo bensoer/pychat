25fc9803047db8029b70144ed4522a9a4790937a
bensoer <ben@soernet.ca>
2017-04-02 14:24:41 -0700

    got railfenccipher working. including dynamic rails

--- crypto/algorithms/railfencecipher.py
+++ crypto/algorithms/railfencecipher.py
@@ -2,6 +2,7 @@ __author__ = 'bensoer'
 
 from crypto.algorithms.algorithminterface import AlgorithmInterface
 from tools.argparcer import ArgParcer
+import math
 
 class RailFenceCipher(AlgorithmInterface):
 
@@ -24,6 +25,7 @@ class RailFenceCipher(AlgorithmInterface):
         if self.__rails == -1:
             # dynamic rails
             railCount = int((len(unencryptedMessage) / 2) - 1)
+            self.logger.debug("Dynamic Rails Selected. RailCount: " + str(railCount))
             for i in range(0, railCount):
                 railList.append(list())
         else:
@@ -35,104 +37,53 @@ class RailFenceCipher(AlgorithmInterface):
 
 
     def encryptString(self, unencryptedMessage):
-
         railList = self.initializeRails(unencryptedMessage)
         if len(railList) == 0:
             return unencryptedMessage.encode()
         else:
 
             # scamble letters into their rails
-            increment = True
-            railIndex = 0
             for index, letter in enumerate(unencryptedMessage):
-                railList[railIndex].append(letter)
+                railList[index % len(railList)].append(letter)
+            self.logger.debug("Rails: " + str(railList))
 
-                if increment:
-                    railIndex += 1
-                    if railIndex >= len(railList)-1:
-                        increment = False
-                else:
-                    railIndex -= 1
-                    if railIndex <= 0:
-                        increment = True
-
-            print(railList)
-
-
-            # read out the message rail by rail
             encryptedMessage = ""
             for rail in railList:
                 for letter in rail:
                     encryptedMessage += letter
 
-        return encryptedMessage.encode()
+            return encryptedMessage.encode()
 
     def decryptString(self, encryptedMessage):
         encryptedMessageText = encryptedMessage.decode()
-        # print(encryptedMessageText)
 
         railList = self.initializeRails(encryptedMessageText)
         if len(railList) == 0:
             return encryptedMessageText
         else:
 
-            # place letters back into their rails
+            # determine needed box size
+            messageLength = len(encryptedMessageText)
+            rowLength = math.ceil(messageLength / len(railList))
+            extraLetters = (messageLength % len(railList))
+            extraSpaces = len(railList) - extraLetters
+            self.logger.debug("Message Length: " + str(messageLength) + " Row Length: " + str(rowLength) + " Extra Spaces: " + str(extraSpaces))
 
-            increment = True
-            railIndex = 0 # resolves to the rail which the letter will be placed in
-            letterIndex = 0 # resolves to the letter in the encryptedMessageText we are trying to find its home to
-            for lap in range(0, len(railList)):
-                # lap resolves to the rail we are currently finding letters that belong to it
-                railIndex = lap
+            letterIndex = 0
+            for index, rail in enumerate(railList):
+                for i in range(0, rowLength):
+                    if extraLetters > 0 and index >= extraLetters and i >= rowLength - 1:
+                        self.logger.debug("Break Point Hit")
+                        break
 
-                for travelIndex in range(lap, len(encryptedMessageText)):
-                    # travelIndex resolves to which letter through the rails we are on - it mostly has no use
-                    # but ensures that the railIndex does not keep going past as many letters as there are in the
-                    # message
-
-                    if lap == railIndex:
-
-                        '''print(lap)
-                        print(railIndex)
-                        print(len(encryptedMessageText))
-                        print(letterIndex)
-                        print(travelIndex)
-                        print(railList)'''
-                        railList[lap].append(encryptedMessageText[letterIndex])
-                        '''print("MATCH MADE. ASSIGNING LETTER: " + str(
-                            encryptedMessageText[letterIndex]) + " TO LAP: " + str(lap))'''
-                        letterIndex += 1
-
-                        if letterIndex >= len(encryptedMessageText):
-                            break
+                    rail.append(encryptedMessageText[letterIndex])
+                    letterIndex += 1
+                    self.logger.debug(railList)
 
 
-                    if increment:
-                        railIndex += 1
-                        if railIndex >= len(railList) - 1:
-                            increment = False
-                    else:
-                        railIndex -= 1
-                        if railIndex <= 0:
-                            increment = True
-
-            print(railList)
-
-
-            # run through rails to reassemble
+            self.logger.debug("RECREATED RAILS: " + str(railList))
             unencryptedMessage = ""
-            increment = True
-            railIndex = 0
-            for letterCount in range(0, len(encryptedMessageText)):
-                unencryptedMessage += railList[railIndex].pop(0)
+            for i in range(0, len(encryptedMessageText)):
+                unencryptedMessage += railList[i % len(railList)].pop(0)
 
-                if increment:
-                    railIndex += 1
-                    if railIndex >= len(railList) - 1:
-                        increment = False
-                else:
-                    railIndex -= 1
-                    if railIndex <= 0:
-                        increment = True
-
-        return unencryptedMessage
+            return unencryptedMessage
