a9220695209db72da4480af31042dfb0213419cf
bensoer <ben@soernet.ca>
2017-04-02 14:31:43 -0700

    added placeholder for pure MD5 implementation

---
+++ crypto/hashes/puremd5.py
@@ -0,0 +1,19 @@
+__author__='bensoer'
+from crypto.hashes.hashinterface import HashInterface
+from Crypto.Hash import MD5 as libmd5
+
+# https://www.ietf.org/rfc/rfc1321.txt
+# https://en.wikipedia.org/wiki/MD5 - example implementation
+
+class PureMD5(HashInterface):
+
+    def hashString(self, stringMessage):
+        md5 = libmd5.new()
+        md5.update(stringMessage.encode())
+        return md5.digest()
+
+    def getDigestSize(self):
+        return 16
+
+    def isValidHash(self, stringMessage, hashBytes):
+        return self.hashString(stringMessage) == hashBytes
